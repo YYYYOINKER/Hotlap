212abfeee2dfb5163fbb1d26d0d9dd88c4a0ccfa
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-07-12 13:20:38 +0200

    Update GameData.gd

--- Scripts/GameData.gd
+++ Scripts/GameData.gd
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d3f99d379ba810440af522adff54bff76783eeba29768ddbe0601f78b4e978e6
-size 75
+extends Node
+
+var selected_map: String = ""
+var selected_car: String = ""
+
