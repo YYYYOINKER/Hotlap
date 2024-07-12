15565fc664d7fcffbcce6a04fad8c7dfd2290046
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-07-12 13:24:37 +0200

    Update hud.gd

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c0711cd84379730676d0d16417a527a9784e445401c5467185b91a05b6d64638
-size 483
+extends Control
+
+# References to the HUD elements
+@onready var steering_wheel = $Wheel
+@onready var gas_pedal = $Gas
+@onready var brake_pedal = $Brakes
+
+# Update function
+func _process(_delta):
+	# Steering Input
+	var steering_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	steering_wheel.rotation_degrees = steering_input * 160  
+	
+	gas_pedal.value = Input.get_action_strength("ui_up")
+	brake_pedal.value = Input.get_action_strength("ui_down")
