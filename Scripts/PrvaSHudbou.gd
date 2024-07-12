ad3d1175a1f52cbb446564c53aa78917cf45ae49
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-07-12 13:22:14 +0200

    Update PrvaSHudbou.gd

--- Scripts/PrvaSHudbou.gd
+++ Scripts/PrvaSHudbou.gd
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:19062d73e302665090da3c606a5e28892d576e39aa9f16d34934c644358ef53d
-size 137
+extends Control
+
+func _process(_delta):
+	MusicController.play_music()
+	get_tree().change_scene_to_file("res://Scenes/MainMenu3D.tscn")
+
+
