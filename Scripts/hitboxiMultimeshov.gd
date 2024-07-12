47d83b855268558f881bd3dcc340517c5982dbee
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-07-12 13:24:25 +0200

    Update hitboxiMultimeshov.gd

--- Scripts/hitboxiMultimeshov.gd
+++ Scripts/hitboxiMultimeshov.gd
@@ -1,3 +1,30 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b152d9800496cc219924704fbe914de08bf72d2adbd19960a2a3b7c930767640
-size 819
+extends MultiMeshInstance3D
+
+func _ready():
+	var multiMesh = get_node("."). multimesh
+	#var Collision_shape = CylinderShape.new()
+	
+	for index in multiMesh.instance_count:
+		var meshTran = multiMesh.get_instance_transform(index)
+		#var shape = CollisionShape.new()
+		var shape = CollisionShape3D.new()
+		var shapeMesh = CylinderMesh.new()
+
+		#shape.shape = multiMesh.mesh.create_trimesh_shape()
+		shape.shape = shapeMesh.create_trimesh_shape()
+
+		shape.transform.basis.x = meshTran.basis.x
+		shape.transform.basis.z = meshTran.basis.z
+		shape.transform.basis.y = meshTran.basis.y
+		shape.scale = Vector3(1,6,1)
+		
+		shape.rotation.y = 25.132
+		shape.rotation.x = 25.132
+		shape.rotation.z = 25.132
+
+		var sBody = StaticBody3D.new()
+		sBody.transform = meshTran
+		sBody.add_child(shape)
+		add_child(sBody)
+
+		index += 1
