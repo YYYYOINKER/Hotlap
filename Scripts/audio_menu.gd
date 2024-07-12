8c7ad70481caefb49e613a49a373101ac2f1889b
Pavol Mihálik <91526804+YYYYOINKER@users.noreply.github.com>
2024-07-12 13:23:05 +0200

    Update audio_menu.gd

--- Scripts/audio_menu.gd
+++ Scripts/audio_menu.gd
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e9906a7aaa3ea6a882fcd078e8b2c1cbc3dead8359bf6a97c694d49627a22b4d
-size 111
+extends Control
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
